f6c128c71e0a2a5b88b92f01a12706c806b91f65
Beshen <56017939+Beshen20@users.noreply.github.com>
2021-04-22 20:06:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,3 +15,121 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+const cbpAnimatedHeader = (function () {
+
+    let docElem = document.documentElement,
+        header = document.querySelector('.header-nav'),
+        headerColor = document.querySelector('.header-nav nav ul li a'),
+        didScroll = false,
+        changeHeaderOn = 900;
+
+    function init() {
+        window.addEventListener('scroll', function (event) {
+            if (!didScroll) {
+                didScroll = true;
+                setTimeout(scrollPage, 0);
+            }
+        }, false);
+    }
+
+    function scrollPage() {
+        const sy = scrollY();
+        if (sy >= changeHeaderOn) {
+            classie.add(header, 'header-fixed');
+            classie.add(headerColor, 'a-color');
+        } else {
+            classie.remove(header, 'header-fixed');
+            classie.remove(headerColor, 'a-color');
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
+
+
+
+/*!
+ * classie - class helper functions
+ 
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+(function (window) {
+
+    'use strict';
+
+
+    function classReg(className) {
+        return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+    }
+
+    // classList support for class management
+    // altho to be fair, the api sucks because it won't accept multiple classes at once
+    let hasClass, addClass, removeClass;
+
+    if ('classList' in document.documentElement) {
+        hasClass = function (elem, c) {
+            return elem.classList.contains(c);
+        };
+        addClass = function (elem, c) {
+            elem.classList.add(c);
+        };
+        removeClass = function (elem, c) {
+            elem.classList.remove(c);
+        };
+    } else {
+        hasClass = function (elem, c) {
+            return classReg(c).test(elem.className);
+        };
+        addClass = function (elem, c) {
+            if (!hasClass(elem, c)) {
+                elem.className = elem.className + ' ' + c;
+            }
+        };
+        removeClass = function (elem, c) {
+            elem.className = elem.className.replace(classReg(c), ' ');
+        };
+    }
+
+    function toggleClass(elem, c) {
+        let fn = hasClass(elem, c) ? removeClass : addClass;
+        fn(elem, c);
+    }
+
+    let classie = {
+        // full names
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        // short names
+        has: hasClass,
+        add: addClass,
+        remove: removeClass,
+        toggle: toggleClass
+    };
+
+    // transport
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(classie);
+    } else {
+        // browser global
+        window.classie = classie;
+    }
+
+})(window);
